d8212cde041e0d6f4b8dc278ca7a5f63de221ca4
Areg-Miqayelyan1 <narek.afrikyan.97@gmail.com>
2020-03-07 12:46:00 +0400

    classwork

---
+++ New folder/work1.py
@@ -0,0 +1,10 @@
+course = "Intro to Python"
+student = "Hripsime Harutyunyan "
+nickname = "hho"
+grade = 10
+python_expert = True
+print("course:" + course)
+print("student:" + student)
+print("nickname:" + nickname)
+print("grade:", grade)
+print("python_expert:",python_expert)

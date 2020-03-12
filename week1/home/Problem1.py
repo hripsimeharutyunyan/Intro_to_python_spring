e8a7095fde05c0de11428de5d5cef2d503521cfa
hripsimeharutyunyan <61898591+hripsimeharutyunyan@users.noreply.github.com>
2020-03-12 21:41:17 +0400

    Add files via upload

---
+++ week1/home/Problem1.py
@@ -0,0 +1,21 @@
+project = "cake"
+difficulty = 4
+ingredients = ["flour","butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print("apples" in ingredients)
+print("butter" in ingredients)
+print(("eggs" in ingredients) or ("margarine" in ingredients))
+print(("eggs"in ingredients) and ("margarine"in ingredients))
+flour= 175
+butter= 175
+sugar="100g"
+eggs=2
+cocoa_powder= "1ts"
+baking_powder= 0.5
+print(type(flour),type(butter),type(sugar),type(eggs), type(cocoa_powder),type(baking_powder))
+print("Flour-",flour,"g")
+print("Butter-",butter,"g")
+print("Sugar- " + sugar)
+print("Eggs-", eggs)
+print("Cocoa powder- " + cocoa_powder)
+print("Baking powder-", baking_powder)
+
